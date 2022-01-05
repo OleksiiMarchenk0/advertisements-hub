d2a5f13145280fd6970c4282aef447b237e96d88
Oleksii <marchenkoolexiy@gmail.com>
2022-01-05 23:58:26 +0100

    Added endpoint for offers

--- python-django/AdvertisementsHub_Django/advertisement_hub/models.py
+++ python-django/AdvertisementsHub_Django/advertisement_hub/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class Category(models.Model):
     def __str__(self):
         return self.name
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=30)
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
     ordering = models.IntegerField()
     class Meta:
         verbose_name="Category"
@@ -15,8 +15,8 @@ class Offer(models.Model):
     def __str__(self):
         return self.name
     category = models.ForeignKey(Category,  on_delete=models.CASCADE, null=True)
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=30)
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
     description = models.TextField(max_length=10000)
     price = models.DecimalField(max_digits=12, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
--- python-django/AdvertisementsHub_Django/advertisement_hub/views.py
+++ python-django/AdvertisementsHub_Django/advertisement_hub/views.py
@@ -6,7 +6,7 @@ from .models import Category, Offer
 from .serializers import CategorySerializer, OfferSerializer
 
 
-# Create your views here.
+#Categories view
 
 @csrf_exempt
 def categoryApi(request, id=0):
@@ -19,17 +19,45 @@ def categoryApi(request, id=0):
         categories_serializers = CategorySerializer(data = category_data)
         if categories_serializers.is_valid():
             categories_serializers.save()
-            return JsonResponse('Added successfully!!',safe=False)
-        return JsonResponse('Failed to add',safe=False)
+            return JsonResponse('Category added successfully!!',safe=False)
+        return JsonResponse('Failed to add category',safe=False)
     elif request.method == 'PUT':
         category_data = JSONParser().parse(request)
         category = Category.objects.get(id=category_data['id'])
         categories_serializers = CategorySerializer(category,data = category_data)
         if categories_serializers.is_valid():
             categories_serializers.save()
-            return JsonResponse('Updated successfully!!',safe=False)
-        return JsonResponse('Failed to update',safe=False)
+            return JsonResponse('Category updated successfully!!',safe=False)
+        return JsonResponse('Failed to update category', safe=False)
     elif request.method == 'DELETE':
        category = Category.objects.get(id=id)
        category.delete()
-       return JsonResponse('Deleted successfully',safe=False)
+       return JsonResponse('Category deleted successfully',safe=False)
+
+#Offers view
+
+@csrf_exempt
+def offersApi(request, id=0):
+    offers=Offer.objects.all()
+    if request.method == 'GET':
+        offers_serializers=OfferSerializer(offers, many=True)
+        return JsonResponse(offers_serializers.data, safe=False)
+    elif request.method == 'POST':
+        offer_data = JSONParser().parse(request)
+        offers_serializers = OfferSerializer(data = offer_data)
+        if offers_serializers.is_valid():
+            offers_serializers.save()
+            return JsonResponse('Offer added successfully!!',safe=False)
+        return JsonResponse('Failed to add offer',safe=False)
+    elif request.method == 'PUT':
+        offer_data = JSONParser().parse(request)
+        offer = Offer.objects.get(id=offer_data['id'])
+        offers_serializers = OfferSerializer(offer,data = offer_data)
+        if offers_serializers.is_valid():
+            offers_serializers.save()
+            return JsonResponse('Offer updated successfully!!',safe=False)
+        return JsonResponse('Failed to update offer',safe=False)
+    elif request.method == 'DELETE':
+        offer = Offer.objects.get(id=id)
+        offer.delete()
+        return JsonResponse('Offer deleted successfully',safe=False)
