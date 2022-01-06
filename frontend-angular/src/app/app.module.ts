e3d299fd824318ee634a1aec6ca4d66dc6e3294b
Oleksii <marchenkoolexiy@gmail.com>
2022-01-06 18:25:40 +0100

    Added all components as empty files and added service with REST

--- frontend-angular/src/app/app.module.ts
+++ frontend-angular/src/app/app.module.ts
@@ -3,16 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { EmployeeComponent } from './employee/employee.component';
+import { ShowEmployeeComponent } from './employee/show-employee/show-employee.component';
+import { CategoryComponent } from './category/category.component';
+import { AddEditComponent } from './category/add-edit/add-edit.component';
+import { ShowCategoryComponent } from './category/show-category/show-category.component';
+import { SharedService } from './shared.service';
+import {HttpClientModule} from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent,
+    ShowEmployeeComponent,
+    CategoryComponent,
+    AddEditComponent,
+    ShowCategoryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [SharedService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
