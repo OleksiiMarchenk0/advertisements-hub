96f72dcc60f06a8e1d3ae8e72c52e4f8b3c50db9
Oleksii <marchenkoolexiy@gmail.com>
2022-01-07 20:01:13 +0100

    removed trash

--- frontend-angular/src/app/app.module.ts
+++ frontend-angular/src/app/app.module.ts
@@ -4,22 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CategoryComponent } from './category/category.component';
-import { AddEditComponent } from './category/add-edit/add-edit.component';
 import { ShowCategoryComponent } from './category/show-category/show-category.component';
 import { SharedService } from './shared.service';
 import {HttpClientModule} from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { OffersComponent } from './offers/offers.component';
-import { ShowOffersComponent } from './offers/show-offers/show-offers.component';
 import { OfferComponent } from './offer/offer.component';
 @NgModule({
   declarations: [
     AppComponent,
     CategoryComponent,
-    AddEditComponent,
     ShowCategoryComponent,
     OffersComponent,
-    ShowOffersComponent,
     OfferComponent
   ],
   imports: [
