578419e8cb13f24a94b8c7f94645a36167bf3800
Oleksii <marchenkoolexiy@gmail.com>
2022-01-08 11:53:42 +0100

    added top nav

--- frontend-angular/src/app/app.module.ts
+++ frontend-angular/src/app/app.module.ts
@@ -9,12 +9,14 @@ import {HttpClientModule} from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { OffersComponent } from './offers/offers.component';
 import { OfferComponent } from './offer/offer.component';
+import { TopNavigationComponent } from './top-navigation/top-navigation.component';
 @NgModule({
   declarations: [
     AppComponent,
     CategoryComponent,
     OffersComponent,
-    OfferComponent
+    OfferComponent,
+    TopNavigationComponent
   ],
   imports: [
     BrowserModule,
